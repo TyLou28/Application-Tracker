434fcca1eb6a18f9253cb5f096916edff8a68699
TyLou28 <72469051+TyLou28@users.noreply.github.com>
2025-03-26 15:56:43 +0000

    Updated User model to include is_active, is_staff

--- users/models.py
+++ users/models.py
@@ -31,6 +31,8 @@ class User(auth_models.AbstractUser):
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     username = None
     
     objects = UserManager()
