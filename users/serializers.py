a5ee2f3f0c4b35fa337f894523360c368b36fe88
TyLou28 <72469051+TyLou28@users.noreply.github.com>
2025-03-26 15:57:14 +0000

    Added create method to serlialisers.py for users

--- users/serializers.py
+++ users/serializers.py
@@ -7,3 +7,13 @@ class userSerialiser(serializers.ModelSerializer):
         fields = '__all__'
         extra_kwargs = {'password': {'write_only': True},
                         'id': {'read_only': True}}
+    
+    def create(self, validated_data):
+        email = validated_data["email"]
+        first_name = validated_data["first_name"]
+        last_name = validated_data["last_name"]
+        password = validated_data["password"]
+
+        user = User.objects.create_user(email=email, first_name=first_name, last_name=last_name, password=password)
+        user.save()
+        return user
