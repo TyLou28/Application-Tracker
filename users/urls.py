b8b234fbd124d5f39099a778ebe8440825439ed3
TyLou28 <72469051+TyLou28@users.noreply.github.com>
2025-03-27 19:56:40 +0000

    added url endpoints for login and logout

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import get_users, register, userRegistrationAPIView
+from .views import get_users, register, userRegistrationAPIView, userLoginAPIView, userLogoutAPIView
 
 urlpatterns = [
     path('users/', get_users, name='get_users'),
     path('users/register', userRegistrationAPIView.as_view(), name='register'),
+    path('users/login', userLoginAPIView.as_view(), name='login'),
+    path('users/logout', userLogoutAPIView.as_view(), name='logout')
 ]
