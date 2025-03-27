148d929a3fb3c9f03681c021c1c8a47d4153c0f3
TyLou28 <72469051+TyLou28@users.noreply.github.com>
2025-03-27 18:30:16 +0000

    registers users to admin site

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import User
+from.forms import userChangeForm, userCreationForm
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+@admin.register(User)
+class adminUser(UserAdmin):
+    add_form = userCreationForm
+    form = userChangeForm
+
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "email"
+    )
+
+    model = User
+    # Change ordering, as my user model doesn't have a username
+    ordering = ['email']
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
