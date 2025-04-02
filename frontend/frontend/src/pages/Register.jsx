93f2308ffa91f57472dd55f1729b9e2e1f61abac
TyLou28 <72469051+TyLou28@users.noreply.github.com>
2025-04-02 13:57:09 +0100

    added registerUser function that connects to backend

--- frontend/frontend/src/pages/Register.jsx
+++ frontend/frontend/src/pages/Register.jsx
@@ -2,8 +2,6 @@ import React, { use, useState } from "react";
 
 export default function Register() {
 
-    const [isLoading, setIsLoading] = useState(false);
-
     const [formData, setFormData] = useState({
         first_name: "",
         last_name: "",
@@ -11,27 +9,67 @@ export default function Register() {
         password1: "",
         password2: "",
     });
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [successMsg, setSuccessMsg] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    const registerUser = async (e) => {
+        e.preventDefault();
+        if(isLoading) {
+            return
+        }
+        const userData = { ...formData };
+        setIsLoading(true);
+        try {
+            const response =  await fetch("http://localhost:8000/users/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(userData),
+            });
+            const data = await response.json();
+            setFormData({
+                first_name: "",
+                last_name: "",
+                email: "",
+                password1: "",
+                password2: "",
+            });
+            setSuccessMsg("Registration Successful")
+        } catch (err) {
+            console.log(err)
+        }
+
+    };
+
     return (
         <div>
             <h2> Register </h2>
             <form>
                 <label>First Name:</label><br/>
                 <input type="text" name="first_name" value={formData.first_name}
-                onChange={(e) => setFormData(e.target.value)}></input><br />
+                placeholder="First name..."
+                onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })}></input><br />
                 <label>Last Name:</label><br/>
                 <input type="text" name="last_name" value={formData.last_name}
-                onChange={(e) => setFormData(e.target.value)}></input><br />
+                placeholder="Last Name..."
+                onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })}></input><br />
                 <label>Email:</label><br/>
                 <input type="email" name="email" value={formData.email}
-                onChange={(e) => setFormData(e.target.value)}></input><br />
+                placeholder="Email..."
+                onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })}></input><br />
                 <label>Password:</label><br/>
                 <input type="password" name="password1" value={formData.password1}
-                onChange={(e) => setFormData(e.target.value)}></input><br />
+                placeholder="Password..."
+                onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })}></input><br />
                 <label>Confirm Password:</label><br/>
                 <input type="password" name="password2" value={formData.password2}
-                onChange={(e) => setFormData(e.target.value)}></input><br />
+                placeholder="Re-Enter Password..."
+                onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })}></input><br />
                 <br />
-                <button type="submit" disabled={isLoading}>Register</button><br />
+                <button type="submit" disabled={isLoading} onClick={registerUser}>Register</button><br />
             </form>
         </div>
     )
