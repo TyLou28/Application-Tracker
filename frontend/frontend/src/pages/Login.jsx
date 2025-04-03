afb4261f80172a7d4b367a572d07e40d62c98424
TyLou28 <72469051+TyLou28@users.noreply.github.com>
2025-04-03 18:49:01 +0100

    Added functionality to login.jsx

--- frontend/frontend/src/pages/Login.jsx
+++ frontend/frontend/src/pages/Login.jsx
@@ -1,7 +1,82 @@
-import React from "react";
+import React, { use, useState } from "react";
 
 export default function Login() {
+
+        const [formData, setFormData] = useState({
+            email: "",
+            password: "",
+        });
+
+        const [isLoading, setIsLoading] = useState(false);
+        const [successMsg, setSuccessMsg] = useState(null);
+        const [error, setError] = useState(null);
+
+        
+    const loginUser = async (e) => {
+        e.preventDefault();
+        if(isLoading) {
+            return
+        }
+        const userData = { ...formData };
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response =  await fetch("http://localhost:8000/users/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(userData),
+            });
+            const data = await response.json();
+
+            if(!response.ok) {
+                setError(data);
+                throw new Error("Login Failed");
+            }
+
+            localStorage.setItem("accessToken", data.tokens.access);
+            localStorage.setItem("refreshToken", data.tokens.refresh);
+
+            setSuccessMsg("Login Successful");
+            setFormData({
+                email: "",
+                password: "",
+            });
+            window.location.href = "/"
+
+        } catch (error) {
+            console.error("Error: ", error);
+        } finally {
+            setIsLoading(false);
+        }
+
+    };
+
     return (
-        <div>Login</div>
+        <div>
+            {error && (
+                <div style={{ color: "red" }}>
+                    {Object.keys(error).map((field, index) => (
+                        <p key={index}>{error[field].join(", ")}</p>
+                    ))}
+                </div>
+            )}
+
+            {successMsg && <p style={{ color: "green" }}>{successMsg}</p>}
+            <h2> Login </h2>
+            <form>
+                <label>Email:</label><br/>
+                <input type="email" name="email" value={formData.email}
+                placeholder="Email..."
+                onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })}></input><br />
+                <label>Password:</label><br/>
+                <input type="password" name="password" value={formData.password}
+                placeholder="Password..."
+                onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })}></input><br />
+                <br />
+                <button type="submit" disabled={isLoading} onClick={loginUser}>Login</button><br />
+            </form>
+        </div>
     )
 }
