a803b593cd38c1bef91f292574332da120dc9ec1
TyLou28 <72469051+TyLou28@users.noreply.github.com>
2025-04-07 17:14:15 +0100

    Added authentication and method to get individual user applications

--- applications/views.py
+++ applications/views.py
@@ -1,6 +1,7 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from .models import Applications
 from .serializers import ApplicationsSerialiser
 
@@ -11,7 +12,9 @@ def get_applications(request):
     return Response(serialiser.data)
 
 @api_view(['POST'])
-def create_applications(request):
+@permission_classes([IsAuthenticated])
+def create_application(request):
+    request.data['user'] = request.user.id
     serialiser = ApplicationsSerialiser(data=request.data)
     if serialiser.is_valid():
         serialiser.save()
@@ -34,3 +37,11 @@ def application_details(request, pk):
             serialiser.save()
             return Response(serialiser.data, status=status.HTTP_201_CREATED)
         return Response(serialiser.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_applications(request):
+    user = request.user
+    applications = Applications.objects.filter(user=user)
+    serializer = ApplicationsSerialiser(applications, many=True)
+    return Response(serializer.data)
