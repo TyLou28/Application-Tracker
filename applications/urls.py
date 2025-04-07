0fd4882b74363038febabae450388222bf49db35
TyLou28 <72469051+TyLou28@users.noreply.github.com>
2025-04-07 17:14:35 +0100

    Added endpoint to get specific user applications

--- applications/urls.py
+++ applications/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import get_applications, create_applications, application_details
+from .views import get_applications, create_application, application_details, user_applications
 
 urlpatterns = [
     path('applications/', get_applications, name='get_applications'),
-    path('applications/create_application', create_applications, name='create_applications'),
+    path('applications/create_application', create_application, name='create_application'),
+    path('user-applications', user_applications, name='user-applications'),
     path('applications/<int:pk>', application_details, name='application_details')
 ]
