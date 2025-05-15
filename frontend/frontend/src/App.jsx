6eb0771c9f82a853e288cb16b024ba36a458104e
TyLou28 <72469051+TyLou28@users.noreply.github.com>
2025-05-15 19:22:52 +0100

    added route for pages

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -2,104 +2,23 @@
 // When the state object changes (data), the component re-renders
 import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import './App.css'
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Layout from './pages/Layout';
-import NewEntry from './pages/NewEntry';
-import TrackApplications from './pages/TrackApplications';
-import Notes from './pages/Notes';
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
 
 
 function App() {
 
-  const [first_name, setFirstName] = useState("")
-  const [isLoggedIn, setLoggedIn] = useState(false)
-
-
-  useEffect(() => {
-      checkLoggedIn();
-      const token = localStorage.getItem("accessToken");
-      if (token) {
-        setLoggedIn(true)
-      }
-  }, []);
-
-    const checkLoggedIn = async () => {
-      try {
-          const token = localStorage.getItem("accessToken");
-
-          if (token) {
-              const response = await fetch("http://localhost:8000/user", {
-                  method: "GET",
-                  headers: {
-                      "Authorization": `Bearer ${token}`,
-                      "Content-Type": "application/json"
-                  }
-              });
-
-              if (!response.ok) {
-                  throw new Error("Failed to authenticate user");
-              }
-
-              const data = await response.json();
-              setLoggedIn(true);
-              setFirstName(data.first_name); // Correctly accessing first name
-          } else {
-              setLoggedIn(false);
-              setFirstName("");
-          }
-      } catch (err) {
-          console.error("Error:", err);
-          setLoggedIn(false);
-          setFirstName("");
-      }
-  };
-
-  const handleLogout = async () => {
-    try {
-      const refreshToken = localStorage.getItem("refreshToken");
-      if (refreshToken) {
-        await fetch("http://localhost:8000/users/logout", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ refresh: refreshToken }),
-        });
-      }
-
-      localStorage.removeItem("accessToken");
-      localStorage.removeItem("refreshToken");
-
-      setLoggedIn(false)
-      setFirstName("")
-      window.location.href = "/"
-
-    }catch (err) {
-      console.error("Logout failed:", err);
-    }
-  };
 
   return (
-    <BrowserRouter>
-      {/* Main Routes */}
-      <Routes>
-        <Route path='/' element={
-          <Layout
-            isLoggedIn={isLoggedIn}
-            first_name={first_name}
-            handleLogout={handleLogout} />}>
-          <Route index element={<Home isLoggedIn={isLoggedIn} first_name={first_name}/>} />
-          <Route path='register' element={<Register />} />
-          <Route path='login' element={<Login />} />
-          <Route path='new-entry' element={<NewEntry />} />
-          <Route path='track-applications' element={<TrackApplications /> } />
-          <Route path='track-applications/applications/:id/notes' element={<Notes />}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        {/* Main Routes */}
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path='*' element={<NotFound />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
