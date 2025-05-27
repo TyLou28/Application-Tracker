c9e064efe1f9a496aafbb08631479ff71e049822
TyLou28 <72469051+TyLou28@users.noreply.github.com>
2025-05-27 11:39:14 +0100

    adding route to register page

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Home } from './pages/Home';
 import { NotFound } from './pages/NotFound';
 import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route index element={<Home />}/>
           <Route path='login' element={<Login />}/>
+          <Route path='register' element={<Register />}/>
           <Route path='*' element={<NotFound />}/>
         </Routes>
       </BrowserRouter>
